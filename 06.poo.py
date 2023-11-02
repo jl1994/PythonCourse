0eae08b91d430e981a4697fa1d394e972475e799
Johan Luna <johan.luna@finkargo.com>
2023-11-02 00:02:29 -0500

    Add two methos to User class

--- 06.poo.py
+++ 06.poo.py
@@ -1,25 +1,33 @@
 class User:  # The first letter is capitalized for classes
-    def __init__(self, username, name, lastname, id):
+    def __init__(self, username, name, lastname, id, email):
         self.username = username
         self.name = name
         self.lastname = lastname
         self.id = id
+        self.email = email
+
+    # New methods
+    def change_password(self):
+        print(f'Change password to {self.username}')
+
+    def create_user(self):
+        print(f'Creating the new user to {self.username}')
 
 
 # We instantiate an object of the User class
-user1 = User("johan.luna", "Johan", "Luna", 1143966442)
-user2 = User("ederlien.bermeo", "Ederlien", "Bermeo", 1143966440)
+user1 = User("johan.luna", "Johan", "Luna",
+             1143966442, "johanluna777@gmail.com")
+user2 = User("ederlien.bermeo", "Ederlien", "Bermeo",
+             1143966440, "ederlien.bermeo@gmail.com")
 
-print(user1.username, "\t", user2.username)
-# print(type(User))
+user1.change_password()
+user2.create_user()
 
 
 class AmazonSES:
     check_service = "Check Endpoint SMTP"
     send_emails = "Send Emails"
     gen_reports = "Generating Reports"
-
-
 check_service_finkargo = AmazonSES()
 
 # print(check_service_finkargo.check_service,
