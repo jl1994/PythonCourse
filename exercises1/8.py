f5ffeb93554997dae83174e23fc59b16dcf99d20
Johan Luna <johan.luna@finkargo.com>
2023-11-20 15:42:46 -0500

    Exercise 8 complete

---
+++ exercises1/8.py
@@ -0,0 +1,24 @@
+# Write a function that allows to user enter a infinity numbers until him enter the word 'stop', then add all the numbers
+
+def numbers_luna():  # Function name
+    numbers = []  # Empty list to save the numbers.
+
+    while True:  # Infinite loop with True
+        # Prompted the user to enter a random number
+        user_input = input('Enter a number: ')
+        # Conditional to validate it what the user entered is and string equal to 'stop'
+        if user_input == 'stop':
+            break
+        try:  # Use this expression to capture and handle exceptions.
+            number = float(user_input)  # Convert string to float
+            numbers.append(number)  # Add elements to list
+        except ValueError:
+            print('Error')
+    total = sum(numbers)  # The sum of the elements
+    return total  # Return sum
+
+
+# It calls a function named numbers_luna and assigns the result of the function
+result = numbers_luna()
+
+print(f'The sum of the elements of the array is: {result}')  # Print result
