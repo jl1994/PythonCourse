07832ef56d44387bec2e0adbedc88dbb113544e5
Johan Luna <johan.luna@finkargo.com>
2023-12-14 11:02:04 -0500

    Query example - 1

--- seasson4/db.py
+++ seasson4/db.py
@@ -1,9 +1,21 @@
+# Import library mysql.connector
 import mysql.connector
 
+# A database instance is created
+conn_db = mysql.connector.connect(
+    user='root', password='r00t2023*!', host='127.0.0.1', port=3306, database='users')
 
-conn_db = mysql.connector.connect(user='root', password='r00t2023*!',
-                                  host='127.0.0.1', port=3306,
-                                  database='users')
+# Create a cursos object
+cursor = conn_db.cursor()
 
+# Query execute
+cursor.execute('select * from users.Users')
+
+# Get the query results
+result_query = cursor.fetchall()
+
+# Print the result
+print(result_query)
+
+# Close connection
 conn_db.close()
-# print(type(conn_db))
